ee245c2f8d63a77b8fd3ff2d8dc4d355bc254f07
HaonanZhang101045166 <59615110+HaonanZhang101045166@users.noreply.github.com>
2023-11-10 17:31:50 -0500

    Nothing special

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../resources/notes';
+
 function App() {
   return (
     <header>
       <Header />
-      <Note />
+      {notes.map((noteItem) => {
+        return (
+          <Note
+            key={noteItem.id}
+            title={noteItem.title}
+            content={noteItem.content}
+          />
+        );
+      })}
       <Footer />
     </header>
   );
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
 
   return (
     <footer>
-      <p>Copyright {year}</p>
+      <p>Copyright ⓒ {year}</p>
     </footer>
   );
 }
